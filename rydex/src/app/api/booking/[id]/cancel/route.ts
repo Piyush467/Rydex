ec67c651f84b15d3fb6400df4848415f37d233c8
Piyush467 <piyushmehrotra61@gmail.com>
2026-06-15 22:51:29 +0530

    created booking cancel api

---
+++ rydex/src/app/api/booking/[id]/cancel/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest } from "next/server";
+import connectDb from "@/lib/db";
+import Booking from "@/models/booking.model";
+import { NextResponse } from "next/server";
+
+
+
+export async function GET(req: NextRequest, context: { params: Promise<{ id: string }> }) {
+    try {
+        const id = (await context.params).id;
+        await connectDb();
+        const booking = await Booking.findById(id);
+        if (!booking || booking.bookingStatus !== "requested") {
+            return NextResponse.json({ message: "Invalid" }, { status: 400 })
+
+        }
+        booking.bookingStatus = "cancelled";
+
+        await booking.save();
+        return NextResponse.json({ success: "true" }, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ message: `cancel booking error ${error}` }, { status: 500 })
+
+    }
+}
